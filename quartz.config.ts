c5fb453577db86231af265837c5b0d28fb4d4b8f
Brenno B. Demarchi <130931374+brennodemarchi@users.noreply.github.com>
2025-02-23 13:03:04 -0300

    Quartz sync: Feb 23, 2025, 1:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Brenno Brandalise Demarchi",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
